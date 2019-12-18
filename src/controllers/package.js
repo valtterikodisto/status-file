ce3003d3f45982d3a86dae45f2d5e9f5532f6a73
Valtteri Kodisto <42912038+valtterikodisto@users.noreply.github.com>
2019-12-18 18:30:42 +0200

    Cached package page rendering

--- src/controllers/package.js
+++ src/controllers/package.js
@@ -2,12 +2,13 @@ const packageRouter = require('express').Router()
 
 packageRouter.get('/:name', (request, response, next) => {
   const packages = request.app.get('packages')
-  const pack = packages.find(pack => pack.name === request.params.name)
+  const { name } = request.params
+  const pack = packages.find(pack => pack.name === name)
   if (!pack) {
     return next()
   }
 
-  response.render('package', pack)
+  response.render('package', { ...pack, cache: true, filename: name })
 })
 
 module.exports = packageRouter
